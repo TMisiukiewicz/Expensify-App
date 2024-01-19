ff22f232e9a722c0c3f267a979d001de9c392521
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-01-19 15:56:25 +0100

    add setup for build

---
+++ src/components/Devtools/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Pressable, Text} from 'react-native';
+import Performance from 'react-native-performance';
+
+const onPress = async () => {
+    console.log('Devtools collecting performance metrics...');
+    // TODO: switch for your local IP
+    try {
+        await fetch('http://192.168.0.106:8080', {
+            method: 'POST',
+            headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify(Performance.getEntries()),
+        });
+    } catch (e) {
+        console.log('Error', e);
+    }
+};
+
+function Devtools() {
+    return (
+        <Pressable
+            onPress={onPress}
+            style={{backgroundColor: 'black', position: 'absolute', bottom: 0, zIndex: 1000, width: '100%'}}
+        >
+            <Text>Send data</Text>
+        </Pressable>
+    );
+}
+
+export default Devtools;
